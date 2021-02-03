9fc23fbfb349334f657eaa8393d09c2528200f67
Ben <me@bpmct.net>
2021-02-03 14:31:08 -0500

    allow custom start dir

--- heroku-entrypoint.sh
+++ heroku-entrypoint.sh
@@ -1,6 +1,8 @@
+# Allow user to aupply a start dir, default to /home/coder/project
+START_DIR=${1:-/home/coder/project}
+
 # Clone the git repo, if it exists
-mkdir /home/coder/project
-[ -z "${GIT_REPO}" ] && echo "No GIT_REPO specified"; git clone $GIT_REPO /home/coder/project
+[ -z "${GIT_REPO}" ] && echo "No GIT_REPO specified"; git clone $GIT_REPO $START_DIR
 
 # Now we can run code-server with the default entrypoint
-/usr/bin/entrypoint.sh --bind-addr 0.0.0.0:8080 $1:/home/coder/project
+/usr/bin/entrypoint.sh --bind-addr 0.0.0.0:8080 $START_DIR
