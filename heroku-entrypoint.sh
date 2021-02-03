984995db51bb9082bd880ad8aa10f2a7beeb662d
Ben <me@bpmct.net>
2021-02-03 13:58:55 -0500

    accept argument in script

--- heroku-entrypoint.sh
+++ heroku-entrypoint.sh
@@ -3,4 +3,4 @@ mkdir /home/coder/project
 [ -z "${GIT_REPO}" ] && echo "No GIT_REPO specified"; git clone $GIT_REPO /home/coder/project
 
 # Now we can run code-server with the default entrypoint
-/usr/bin/entrypoint.sh --bind-addr 0.0.0.0:8080 /home/coder/project
+/usr/bin/entrypoint.sh --bind-addr 0.0.0.0:8080 $1:/home/coder/project
